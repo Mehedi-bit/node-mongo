865507ce2b29875457b5775069313ea0ba09afd5
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2021-12-26 12:19:45 +0600

    node simply done

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("I know how to open Node. Thank you for calling me... YAY");
+    fruit = {
+        product: 'ada',
+        price: 220
+    }
+    res.send(fruit);
 })
 
-app.listen(8080, console.log("Listening to port 8080"));
+app.get('/fruits/banana', (req, res) => {
+    res.send({name: 'banana', id: 1, class: 'sagor kola', quantity: 1000, price: 50000});
+})
+
+const users = ['Emon', 'Tapu', 'Sunny', 'Hridoy', 'Nisar'];
+
+app.get('/users/:id', (req, res) => {
+    console.log(req.params)
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});       // will return an object in json format  (api)
+    console.log(name);
+})
+
+app.listen(4080, console.log("Listening to port 4080"));
